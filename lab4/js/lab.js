a41ea2e0a8d9b8a6a7028ef4e23e2464a2877dab
carobowen <166079089+carobowen@users.noreply.github.com>
2024-04-25 20:43:52 -0700

    lab 5 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Tic-tac-toe game implementation 
+// Author: Caroline Bowen
+// Date: 23 April 2024 
+//Print the Board Before Each Turn
+    //Print the rows 
+        //Print the lines 
+//Player One  Makes Their First Move
+    //Decide To be X or O
+        //Their personal Server Sends A Message to the Server of the Game They’re Playing
+            //Request is Processed and player 1’s choice shows up on-screen
+//Player Two Responds to Player Ones Move 
+    //Their Personal Server Sends A Message to the Server of the Game They’re Playing and Player 1’s 
+        // Request is Processed and player 2’s choice shows up on-screen
+//Game Continues Until One Player Has Filled a Row In a Straight or Diagonal Line With Their Letter (deeming them the winner)
 
 // Constants
 
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables 
+// Author: Caroline Bowen
+// Date: April 25, 2024 
+//
+
+//Define Variables 
+make = "Nissian";
+model = "Rogue";
+color = "Red";
+year = "2013";
+
+
+// calculate
+age = 2024 - year;
+
+
+// output 
+document.writeln("Make: " + make + "<br>"); 
+document.writeln("Model:" + model +"<br>") ;
+document.writeln("Color:" + color +"<br>") ;
+document.writeln("Year:" + year + "<br>") ;
+document.writeln("Age:" + age + " years<br>");
+
+
 
 // Constants
 
